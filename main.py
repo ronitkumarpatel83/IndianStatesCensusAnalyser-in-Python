4756548e21651a962e5e48521c15a06dee744072
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-06-25 15:42:00 +0530

    [Ronit]Added : Welcome message

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+    @Name = Ronit Kumar Patel
+    @Title = Indian States Census Analyser
+"""
+
+print("*************************************************************************")
+print("             Welcome to Indian States Census Analyser Program            ")
+print("*************************************************************************")
+
